1e7117068972e879f4a18a5a9cd178e8657a2b90
joshpf <panflop@ozemail.com.au>
2020-09-10 22:30:53 +1000

    update - added more stuff

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-from setuptools import setup
-setup(name='surge',
-      version='0.1',
-    #   description='The funniest joke in the world',
-    #   url='http://github.com/storborg/funniest',
-    #   author='Flying Circus',
-    #   author_email='flyingcircus@example.com',
-    #   license='',
-      packages=['surge'],
-    #   zip_safe=False
-      )
+
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="surgetypes-python", # Replace with your own username
+    version="0.0.1",
+    author="Joshua p-f",
+    author_email="jporrittfraser@gmail.com",
+    description="Automated type checking",
+    long_description="Automated type checking",
+    long_description_content_type="text/markdown",
+    url="https://github.com/joshp-f/surgetypes-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
